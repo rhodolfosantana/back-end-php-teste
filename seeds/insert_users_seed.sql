2f25028481ea9a3d2cfea2df336976a3cdbd55fb
Melhor Comunicação <49531976+melhorcom@users.noreply.github.com>
2019-07-08 16:22:44 -0300

    Update insert_users_seed.sql

--- seeds/insert_users_seed.sql
+++ seeds/insert_users_seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO users(name, email)
     VALUES
-        ('Foo Bar', 'foo@bar'),
-        ('Bar Baz', 'bar@baz'),
-        ('Baz Foo', 'baz@foo')
+        ('Foo Bar', 'foo@bar', 10, 81999999999),
+        ('Bar Baz', 'bar@baz', 20, 81999999999),
+        ('Baz Foo', 'baz@foo', 15, 81912349876)
