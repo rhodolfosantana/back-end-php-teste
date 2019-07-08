514a4d9587a2b8b2ca685e9921299ac225fa92fa
Melhor Comunicação <49531976+melhorcom@users.noreply.github.com>
2019-07-08 16:19:52 -0300

    Update create_table_users.sql

--- migrations/create_table_users.sql
+++ migrations/create_table_users.sql
@@ -2,7 +2,7 @@ create table users(
     id int NOT NULL AUTO_INCREMENT,
     name varchar(100) NOT NULL,
     email varchar(100) NOT NULL,
-    idade int NOT NULL,
-    fone int NOT NULL,
+    idade NUMBER NOT NULL,
+    fone NUMBER NOT NULL,
     PRIMARY KEY (id)
 )
